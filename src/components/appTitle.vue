d9057c92797ac6679fb8d1fff9792ca5d530cdb4
DavideGila <davide.gila@gmail.com>
2023-11-08 14:50:49 +0100

    created a return that shows the title

---
+++ src/components/appTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ titolo }}</h1>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            titolo: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
