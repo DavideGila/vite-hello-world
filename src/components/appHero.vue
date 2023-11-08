18fe74f66d846bb7d550e90b0e4e956f46527d65
DavideGila <davide.gila@gmail.com>
2023-11-08 15:46:29 +0100

    added an image in return

---
+++ src/components/appHero.vue
@@ -0,0 +1,17 @@
+<template>
+    <img :src='img'>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            img: '../../public/images/Edward_Elric.jpg'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
