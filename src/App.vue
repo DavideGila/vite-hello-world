4cfe8e1e47fd0caf1f8a7fae6118957f78ab09de
DavideGila <davide.gila@gmail.com>
2023-11-08 15:47:44 +0100

    imported appHero for the Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <appTitle/>
+  <appHero/>
 </template>
 
 <script>
 import appTitle from './components/appTitle.vue'
+import appHero from './components/appHero.vue'
 
 export default {
   components: {
-      appTitle
+      appTitle,
+      appHero
     },
   data () {
     return {}
